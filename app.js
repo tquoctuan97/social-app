01a67cf6ad42d0383eb31799322d1a36bb84efce
TuanTQ <tquoctua97@gmail.com>
2021-08-14 13:55:42 +0700

    init complex-app

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'))
+app.set('views','views')
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res) {
+    res.render('guest-home')
+})
+
+app.listen(3000)
