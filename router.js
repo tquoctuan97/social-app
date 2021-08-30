504bf6f705e2a5b1a4b2d26b13fcda3679bc9c7e
TuanTQ <tquoctua97@gmail.com>
2021-08-30 20:19:25 +0700

    Letting User Create Posts - Part 1

--- router.js
+++ router.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("./controllers/userController");
+const postController = require("./controllers/postController");
 
 router.get("/", userController.home);
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
+
+router.get(
+  "/create-post",
+  userController.mustBeLoggedIn,
+  postController.viewCreateScreen
+);
 
 module.exports = router;
