9e5a9cfe51780e61e4ac2bd113df8f0094e7795d
TuanTQ <tquoctua97@gmail.com>
2021-09-02 16:09:49 +0700

    Viewing a Post (Part 2)

--- controllers/postController.js
+++ controllers/postController.js
@@ -26,6 +26,6 @@ exports.viewSinglePost = async function (req, res) {
     let post = await Post.findSingleById(req.params.id);
     res.render("single-post-screen", { post: post });
   } catch {
-    res.send("404 template will go here");
+    res.render("404");
   }
 };
