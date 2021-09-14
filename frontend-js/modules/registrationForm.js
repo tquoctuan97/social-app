760176a77c7a4762b35c4a7cf3e2871713dc066c
TuanTQ <tquoctua97@gmail.com>
2021-09-14 20:03:22 +0700

    Validation Register Password Field

--- frontend-js/modules/registrationForm.js
+++ frontend-js/modules/registrationForm.js
@@ -7,6 +7,8 @@ export default class RegistrationForm {
     this.username.previousValue = '';
     this.email = document.querySelector('#email-register');
     this.email.previousValue = '';
+    this.password = document.querySelector('#password-register');
+    this.password.previousValue = '';
     this.events();
     this.insertValidationElements();
   }
@@ -18,6 +20,9 @@ export default class RegistrationForm {
     });
     this.email.addEventListener('keyup', () => {
       this.isDifferent(this.email, this.emailHandler);
+    });
+    this.password.addEventListener('keyup', () => {
+      this.isDifferent(this.password, this.passwordHandler);
     });
   }
 
@@ -107,6 +112,35 @@ export default class RegistrationForm {
         });
     }
   }
+
+  // Method Validation Password
+
+  passwordHandler() {
+    this.password.errors = false;
+    this.passwordImmediately();
+    clearTimeout(this.password.timer);
+    this.password.timer = setTimeout(() => {
+      this.passwordAfterDelay();
+    }, 800);
+  }
+
+  passwordImmediately() {
+    if (this.password.value.length > 50) {
+      this.showValidationError(this.password, 'Password cannot exceed 50 characters.');
+    }
+
+    if (!this.password.errors) {
+      this.hideValidationError(this.password);
+    }
+  }
+
+  passwordAfterDelay() {
+    if (this.password.value.length < 12) {
+      this.showValidationError(this.password, 'Password must be at least 12 characters.');
+    }
+  }
+
+  // Method Show/Hide Validation
 
   showValidationError(el, message) {
     el.nextElementSibling.innerHTML = message;
