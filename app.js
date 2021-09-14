8456beae4a298789e0a5f4eada06507cea45a284
TuanTQ <tquoctua97@gmail.com>
2021-09-14 21:08:00 +0700

    Preventing Cross-Site Request Forgery (CSRF)

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const MongoStore = require('connect-mongo')(session);
 const flash = require('connect-flash');
 const markdown = require('marked');
 const sanitizeHTML = require('sanitize-html');
+const csrf = require('csurf');
 const app = express();
 
 let sessionOptions = session({
@@ -46,11 +47,29 @@ app.use(express.json());
 app.use(express.static('public'));
 app.set('views', 'views');
 app.set('view engine', 'ejs');
+app.use(csrf());
+
+app.use(function (req, res, next) {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 app.use('/', router);
 
-const server = require('http').createServer(app);
+app.use(function (err, req, res, next) {
+  if (err) {
+    if ((err.code = 'EBADCRFTOKEN')) {
+      req.flash('errors', 'Cross site request forgery detected.');
+      req.session.save(function () {
+        res.redirect('/');
+      });
+    } else {
+      res.redirect('404');
+    }
+  }
+});
 
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 io.use(function (socket, next) {
