c8a1d626f0d75e0163e2a3ea49dc15115e534531
TuanTQ <tquoctua97@gmail.com>
2021-09-25 20:43:21 +0700

    fixed syntax

--- router-api.js
+++ router-api.js
@@ -1,30 +1,30 @@
 const express = require('express');
-const routerApi = express.Router();
+const apiRouter = express.Router();
 const userController = require('./controllers/userController');
 const postController = require('./controllers/postController');
 const followController = require('./controllers/followController');
 const cors = require('cors');
 
-routerApi.use(cors());
+apiRouter.use(cors());
 
 // user related route
-routerApi.post('/login', userController.apiLogin);
-routerApi.post('/register', userController.apiRegister);
+apiRouter.post('/login', userController.apiLogin);
+apiRouter.post('/register', userController.apiRegister);
 
 // post related routes
-routerApi.get('/post/:id', postController.apiGetSinglePost);
-routerApi.post('/create-post', userController.apiMustBeLoggedIn, postController.apiCreatePost);
-routerApi.delete('/post/:id', userController.apiMustBeLoggedIn, postController.apiDelete);
+apiRouter.get('/post/:id', postController.apiGetSinglePost);
+apiRouter.post('/create-post', userController.apiMustBeLoggedIn, postController.apiCreatePost);
+apiRouter.delete('/post/:id', userController.apiMustBeLoggedIn, postController.apiDelete);
 
 // profile related routes
-routerApi.get(
+apiRouter.get(
   '/profile/:username',
   userController.ifUserExists,
   userController.shareProfile,
   userController.profileBasicData
 );
-routerApi.get('/profile/:username/posts', userController.apiGetPostsByUsername);
+apiRouter.get('/profile/:username/posts', userController.apiGetPostsByUsername);
 apiRouter.get('/profile/:username/followers', userController.ifUserExists, userController.profileFollowers);
 apiRouter.get('/profile/:username/following', userController.ifUserExists, userController.profileFollowing);
 
-module.exports = routerApi;
+module.exports = apiRouter;
